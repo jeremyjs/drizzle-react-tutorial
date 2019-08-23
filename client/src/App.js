3262d6ece2399bcc883e816028168f5f460357b5
Jeremy Meyer <jeremypatrickmeyer@gmail.com>
2019-08-22 22:05:35 -0700

    update index.js and App component to subscribe to drizzle store

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
+const App = (props) => {
+  // const state = { loading: true, drizzle_state: null }
+  const [isLoading, setIsLoading] = useState(true)
+  const [drizzleState, setDrizzleState] = useState(null)
+
+  useEffect(() => {
+    const { drizzle } = props
+  
+    // subscribe to changes in the store
+    const unsubscribe = drizzle.store.subscribe(() => {
+  
+      // every time the store updates, grab the state from drizzle
+      const drizzle_state = drizzle.store.getState()
+  
+      // check to see if it's ready, if so, update local component state
+      if (drizzle_state.drizzleStatus.initialized) {
+        // this.setState({ loading: false, drizzle_state })
+        setIsLoading(false)
+        setDrizzleState(drizzle_state)
+      }
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  })
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Example app with Drizzle and React
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="#"
           rel="noopener noreferrer"
         >
-          Learn React
+          {isLoading ? 'Loading Drizzle...' : 'Drizzle is ready'}
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import drizzle functions and contract artifact
+import { Drizzle, generateStore } from 'drizzle'
+import StringStore from './contracts/StringStore.json'
+
+// let drizzle know what contracts we want and how to access our test blockchain
+const options = {
+  contracts: [StringStore],
+  web3: {
+    fallback: {
+      type: 'ws',
+      url: 'ws://127.0.0.1:9545',
+    },
+  },
+}
+
+// setup the drizzle store and drizzle
+const drizzle = new Drizzle(options)
+
+ReactDOM.render(<App drizzle={drizzle} />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
